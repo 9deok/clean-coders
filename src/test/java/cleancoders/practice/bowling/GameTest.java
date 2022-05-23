064ccb7e4cb0e54653ec8f743024f51d9efaa618
9deok <butterflynine20@gmail.com>
2022-05-23 22:05:08 +0900

    볼링게임 마무리

--- src/test/java/cleancoders/practice/bowling/GameTest.java
+++ src/test/java/cleancoders/practice/bowling/GameTest.java
@@ -1,5 +1,7 @@
 package cleancoders.practice.bowling;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,58 @@ public class GameTest {
     public void canRoll() {
         game.roll(0);
     }
+
+    @Test
+    public void gutterGame() {
+        rollMany(0,20);
+        assertThat(game.getScore()).isEqualTo(0);
+    }
+
+    @Test
+    public void allOnes() {
+        rollMany(1,20);
+        assertThat(game.getScore()).isEqualTo(20);
+    }
+
+    @Test
+    public void oneSpare() {
+        rollSpare();
+        game.roll(3);
+        rollMany(17,0);
+        assertThat(game.getScore()).isEqualTo(16);
+    }
+
+    @Test
+    public void oneStrike() {
+        rollStrike();
+        game.roll(5);
+        game.roll(3);
+        rollMany(16, 0);
+        assertThat(game.getScore()).isEqualTo(26);
+    }
+
+    @Test
+    public void perfectGame() {
+        rollMany(10,10);
+        game.roll(10);
+        game.roll(10);
+        assertThat(game.getScore()).isEqualTo(300);
+    }
+
+    private void rollStrike() {
+        game.roll(10);
+    }
+
+    private void rollSpare() {
+        game.roll(5);
+        game.roll(5);
+    }
+
+    private void rollMany(int pins, int frames) {
+        for (int i = 0; i < frames; i++) {
+            game.roll(pins);
+        }
+    }
+
 
 }
